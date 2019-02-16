956cce949f9345e2424c52ad37757ebab6b8eb4a
Johan van Rhyn <github@jvanrhyn.co.za>
2019-02-16 23:15:07 +0200

    Added a test to test travis testing

---
+++ src/Jobcard.Web.Tests/Services/Data/DefaultsTests.cs
@@ -0,0 +1,38 @@
+﻿using Jobcard.Models;
+using Jobcard.Services.Data;
+using Shouldly;
+using System.Linq;
+using Xunit;
+
+namespace Jobcard.Web.Tests.Services.Data
+{
+    public class DefaultsTests
+    {
+        [Fact]
+        public void BasicTest()
+        {
+            DefaultsManager defaultsManager = new DefaultsManager();
+            defaultsManager.SetDatabase(@"TestDatabase");
+
+            LayerKindDefault layerKindDefault = new LayerKindDefault
+            {
+                Acceleration = 200,
+                Blower = true,
+                Corner = 200,
+                LayerType = LayerType.Cut,
+                Name = "Test",
+                Power1 = 55,
+                Power1Min = 50,
+                Power2 = 80,
+                Power2Min = 60
+            };
+
+            defaultsManager.GetAll().Count().ShouldBe(0);
+
+            var result = defaultsManager.Add(layerKindDefault);
+            result.Id.ShouldNotBe(0);
+            
+            defaultsManager.GetAll().Count().ShouldBe(1);
+        }
+    }
+}
