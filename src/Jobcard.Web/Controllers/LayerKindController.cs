dc01b58f81923d4e8e5d7aa924c4f0eaae180c2a
Johan van Rhyn <jvanrhyn@users.noreply.github.com>
2019-02-21 21:22:12 +0200

    Feature/database (#4)
    * Added HttpPolicy assembly
    
    * Added a test to test travis testing
    
    * Test a project, not a dll
    
    * Added VSCode configuration files
    
    * Added layer kinds
    
    And my first, copied, React view
    
    * Lock files changed
    
    * Broken test, fixed

---
+++ src/Jobcard.Web/Controllers/LayerKindController.cs
@@ -0,0 +1,58 @@
+﻿using Jobcard.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jobcard.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class LayerKindController : ControllerBase
+    {
+        public LayerKindController()
+        {
+
+        }
+
+        [HttpGet()]
+        public ActionResult<IEnumerable<LayerKindDefault>> GetAll()
+        {
+            var result = new List<LayerKindDefault>
+            {
+                new LayerKindDefault
+                {
+                    Acceleration = 100,
+                    Blower = true,
+                    Corner = 100,
+                    LayerType = LayerType.Cut,
+                    Name = "3mm MDF Cut",
+                    Power1 = 50,
+                    Power1Min = 50,
+                    Power2 = 50,
+                    Power2Min = 50,
+                    ScanGap = 0.065m,
+                    Speed = 100
+
+                },
+                new LayerKindDefault
+                {
+                    Acceleration = 100,
+                    Blower = true,
+                    Corner = 100,
+                    LayerType = LayerType.Cut,
+                    Name = "6mm MDF Cut",
+                    Power1 = 50,
+                    Power1Min = 50,
+                    Power2 = 50,
+                    Power2Min = 50,
+                    ScanGap = 0.065m,
+                    Speed = 100
+
+                }
+            };
+
+            return Ok(result);
+        }
+    }
+}
